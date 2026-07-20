7eda9afab76153d17d06f729b99f927d41535074
Sathish <rsathishikumar3@gmail.com>
2026-07-20 23:53:19 +0530

    changes

---
+++ ChromeExtension/notes.js
@@ -0,0 +1,6 @@
+// For https://wallpapercave.com/
+let images = [...document.querySelectorAll("a.wpinkw img")]
+images.map(image => image.src)
+images
+
+
---
+++ ChromeExtension/script.js
@@ -0,0 +1,44 @@
+async function downloadImages() {
+  let urls = document.querySelector("#urls").value;
+  if (urls.length == 0) return
+  console.log("Before parsing", urls)
+  try {
+    urls = JSON.parse(urls)
+  } catch (err) {
+    console.error(err)
+    return
+  }
+
+  console.log("After parsing:", urls)
+
+  const response = await triggerPythonDaemon(urls);
+
+  const messageElement = document.querySelector("#message");
+  messageElement.style.display = "block"
+  if (response) {
+    messageElement.innerHTML = "Downloading in progress check the configured downlaod folder"
+  } else {
+    messageElement.innerHTML = "Something went wrong, downloading failed"
+  }
+}
+
+function triggerPythonDaemon(urls) {
+  return new Promise((resolve, reject) => {
+    fetch("http://127.0.0.1:8766/download_images", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({"urls": urls}),
+    })
+      .then(() => resolve(true))
+      .catch((err) => {
+        console.error(err);
+        return resolve(false);
+      });
+  })
+}
+
+document
+    .getElementById("download-btn")
+    .addEventListener("click", downloadImages);
